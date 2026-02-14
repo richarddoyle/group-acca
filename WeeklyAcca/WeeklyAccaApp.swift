d3d830b685813ccee2ac84b94bd4f8946b2f1692
Richard Doyle <richard.doyle.87@gmail.com>
2026-02-13 23:33:56 -0800

    Initial Commit

---
+++ WeeklyAcca/WeeklyAccaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyAccaApp.swift
+//  WeeklyAcca
+//
+//  Created by Richard Doyle on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyAccaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
